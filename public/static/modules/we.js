222f87194926148e25223be1e085cfc97ca66aa5
qinxuening <2423859713@qq.com>
2018-09-24 14:58:29 +0800

    we.js

---
+++ public/static/modules/we.js
@@ -0,0 +1,219 @@
+/**
+ * Created by qinxuening on 2018/9/24.
+ */
+layui.define(['jquery', 'layer'], function(exports){
+    $ = layui.jquery;
+    layer = layui.layer;
+    var we = {
+        events: {
+            //请求成功的回调
+            onAjaxSuccess: function (ret, onAjaxSuccess) {
+                var data = typeof ret.data !== 'undefined' ? ret.data : null;
+                var msg = typeof ret.msg !== 'undefined' && ret.msg ? ret.msg : '操作完成';
+                if (typeof onAjaxSuccess === 'function') {
+                    var result = onAjaxSuccess.call(this, data, ret);
+                    if (result === false)
+                        return;
+                }
+                layer.msg(msg);
+            },
+
+            //请求错误的回调
+            onAjaxError: function (ret, onAjaxError) {
+                var data = typeof ret.data !== 'undefined' ? ret.data : null;
+                if (typeof onAjaxError === 'function') {
+                    var result = onAjaxError.call(this, data, ret);
+                    if (result === false) {
+                        return;
+                    }
+                }
+                layer.msg(ret.msg);
+            },
+            //服务器响应数据后
+            onAjaxResponse: function (response) {
+                try {
+                    var ret = typeof response === 'object' ? response : JSON.parse(response);
+                    if (!ret.hasOwnProperty('code')) {
+                        $.extend(ret, {code: -2, msg: response, data: null});
+                    }
+                } catch (e) {
+                    var ret = {code: -1, msg: e.message, data: null};
+                }
+                return ret;
+            }
+        },
+        api: {
+            //发送Ajax请求
+            ajax: function (options, success, error) {
+                options = typeof options === 'string' ? {url: options} : options;
+                var index = Layer.load();
+                // return console.log(options);
+                //jQuery.extend() 函数用于将一个或多个对象的内容合并到目标对象
+                options = $.extend({
+                    type: "POST",
+                    dataType: "json",
+                    success: function (ret) {
+                        Layer.close(index);
+                        ret = we.events.onAjaxResponse(ret);
+
+                        if (ret.code === 1) {
+                            we.events.onAjaxSuccess(ret, success);
+                        } else {
+                            we.events.onAjaxError(ret, error);
+                        }
+                    },
+                    error: function (xhr) {
+                        Layer.close(index);
+                        var ret = {code: xhr.status, msg: xhr.statusText, data: null};
+                        we.events.onAjaxError(ret, error);
+                    }
+                }, options);
+                $.ajax(options);
+            },
+
+            //打开一个弹出窗口
+            open: function (url, title, options,windowSize) {
+                title = title ? title : "";
+                if (windowSize != null) {
+                    var area = windowSize;
+                } else {
+                    var area = [$(window).width() > 800 ? '800px' : '95%', $(window).height() > 600 ? '600px' : '95%'];
+                }
+                options = $.extend({
+                    type: 2,
+                    title: title,
+                    shadeClose: true,
+                    shade: 0.6,
+                    maxmin: true,
+                    moveOut: true,
+                    area: area,
+                    content: url,
+                    skin: 'layui-layer-we',
+                    zIndex: Layer.zIndex,
+                    success: function (layero, index) {
+                        var that = this;
+                        //存储callback事件
+                        $(layero).data("callback", that.callback);
+                        //$(layero).removeClass("layui-layer-border");
+                        Layer.setTop(layero);
+                        var frame = Layer.getChildFrame('html', index);
+                        var layerfooter = frame.find(".layer-footer");
+                        Fast.api.layerfooter(layero, index, that);
+
+                        //绑定事件
+                        if (layerfooter.size() > 0) {
+                            // 监听窗口内的元素及属性变化
+                            // Firefox和Chrome早期版本中带有前缀
+                            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+                            // 选择目标节点
+                            var target = layerfooter[0];
+                            // 创建观察者对象
+                            var observer = new MutationObserver(function (mutations) {
+                                we.api.layerfooter(layero, index, that);
+                                mutations.forEach(function (mutation) {
+                                });
+                            });
+                            // 配置观察选项:
+                            var config = {attributes: true, childList: true, characterData: true, subtree: true}
+                            // 传入目标节点和观察选项
+                            observer.observe(target, config);
+                            // 随后,你还可以停止观察
+                            // observer.disconnect();
+                        }
+
+                    }
+                }, options ? options : {});
+                if ($(window).width() < 480 || (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream && top.$(".tab-pane.active").size() > 0)) {
+                    options.area = [top.$(".tab-pane.active").width() + "px", top.$(".tab-pane.active").height() + "px"];
+                    options.offset = [top.$(".tab-pane.active").scrollTop() + "px", "0px"];
+                }
+                // return console.log(options);
+                return layer.open(options);
+            },
+            //关闭窗口并回传数据
+            close: function (data) {
+                var index = parent.layer.getFrameIndex(window.name);
+                var callback = parent.$("#layui-layer" + index).data("callback");
+                //再执行关闭
+                parent.layer.close(index);
+                //再调用回传函数
+                if (typeof callback === 'function') {
+                    callback.call(undefined, data);
+                }
+            },
+            layerfooter: function (layero, index, that) {
+                var frame = Layer.getChildFrame('html', index);
+                var layerfooter = frame.find(".layer-footer");
+                if (layerfooter.size() > 0) {
+                    $(".layui-layer-footer", layero).remove();
+                    var footer = $("<div />").addClass('layui-layer-btn layui-layer-footer');
+                    footer.html(layerfooter.html());
+                    if ($(".row", footer).size() === 0) {
+                        $(">", footer).wrapAll("<div class='row'></div>");
+                    }
+                    footer.insertAfter(layero.find('.layui-layer-content'));
+                    //绑定事件
+                    footer.on("click", ".btn", function () {
+                        if ($(this).hasClass("disabled") || $(this).parent().hasClass("disabled")) {
+                            return;
+                        }
+                        $(".btn:eq(" + $(this).index() + ")", layerfooter).trigger("click");
+                    });
+
+                    var titHeight = layero.find('.layui-layer-title').outerHeight() || 0;
+                    var btnHeight = layero.find('.layui-layer-btn').outerHeight() || 0;
+                    //重设iframe高度
+                    $("iframe", layero).height(layero.height() - titHeight - btnHeight);
+                }
+                //修复iOS下弹出窗口的高度和iOS下iframe无法滚动的BUG
+                if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+                    var titHeight = layero.find('.layui-layer-title').outerHeight() || 0;
+                    var btnHeight = layero.find('.layui-layer-btn').outerHeight() || 0;
+                    $("iframe", layero).parent().css("height", layero.height() - titHeight - btnHeight);
+                    $("iframe", layero).css("height", "100%");
+                }
+            },
+            success: function (options, callback) {
+                var type = typeof options === 'function';
+                if (type) {
+                    callback = options;
+                }
+                return Layer.msg('操作完成', $.extend({
+                    offset: 0, icon: 1
+                }, type ? {} : options), callback);
+            },
+            error: function (options, callback) {
+                var type = typeof options === 'function';
+                if (type) {
+                    callback = options;
+                }
+                return layer.msg('操作失败', $.extend({
+                    offset: 0, icon: 2
+                }, type ? {} : options), callback);
+            },
+            layer: layer
+        },
+        init: function () {
+            // 对相对地址进行处理
+            layer.config({
+                skin: 'layui-layer-fast'
+            });
+            // 绑定ESC关闭窗口事件
+            $(window).keyup(function (e) {
+                if (e.keyCode == 27) {
+                    if ($(".layui-layer").size() > 0) {
+                        var index = 0;
+                        $(".layui-layer").each(function () {
+                            index = Math.max(index, parseInt($(this).attr("times")));
+                        });
+                        if (index) {
+                            layer.close(index);
+                        }
+                    }
+                }
+            });
+        }
+    };
+    we.init();
+    exports('authtree', we);
+});
