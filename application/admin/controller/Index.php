9f48bc3e2104faee92ca9ff3ba31e7cb04a24b5c
qinxuening <2423859713@qq.com>
2018-10-07 23:17:27 +0800

    登陆模块

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,6 +6,10 @@
  */
 namespace app\admin\controller;
 use app\common\controller\baseAdmin;
+use think\Config;
+use think\Hook;
+use think\Validate;
+use think\Db;
 
 class Index extends baseAdmin {
     
@@ -32,6 +36,99 @@ class Index extends baseAdmin {
         return $this->view->fetch();
     }
     
+    /**
+     * 管理员登录
+     */
+    public function login()
+    {
+        $url = $this->request->get('url', 'index/index');
+        $rule_info = get_passwd_rule();
+        $login_captcha = Db::name('config')->where(['key' => 'login_captcha'])->column('value')[0];
+    
+        if ($this->auth->isLogin())
+        {
+            $this->success(__("You've logged in, do not login again"), $url);
+        }
+        if ($this->request->isPost())
+        {
+            $username = $this->request->post('username');
+            $password = $this->request->post('password');
+            $keeplogin = $this->request->post('keeplogin');
+            $token = $this->request->post('__token__');
+    
+            $pass_regex = 'regex:'.$rule_info[0];
+    
+            $rule = [
+                'username'  => 'require|length:3,30',
+                'password'  => 'require|'.$pass_regex,
+            ];
+    
+            $message = [
+                'password.regex' => $rule_info[1]
+            ];
+    
+            $data = [
+                'username'  => $username,
+                'password'  => $password,
+            ];
+
+            if($login_captcha)
+            {
+                $rule['captcha'] = 'require|captcha';
+                $data['captcha'] = $this->request->post('captcha');
+            }
+    
+    
+            $validate = new Validate($rule, $message, ['username' => __('Username'), 'password' => __('Password'), 'captcha' => __('Captcha')]);
+    
+            $result = $validate->check($data);
+    
+            if (!$result)
+            {
+                $this->error($validate->getError(), $url, ['token' => $this->request->token()]);
+            }
+    
+            $result = $this->auth->login($username, $password, $keeplogin ? 86400 : 0);
+            if ($result === true)
+            {
+                $this->success(__('Login successful'), $url, ['url' => $url, 'id' => $this->auth->id, 'username' => $username, 'avatar' => $this->auth->avatar]);
+            }
+            else
+            {
+                $msg = $this->auth->getError();
+                $msg = $msg ? $msg : __('Username or password is incorrect');
+                $this->error($msg, $url, ['token' => $this->request->token()]);
+            }
+        }
+    
+        $result = $this->auth->login('admin', '12345678', $keeplogin ? 86400 : 0);
+        if ($result === true)
+            {
+                $this->redirect('/');
+            }
+
+        // 根据客户端的cookie,判断是否可以自动登录
+        if ($this->auth->autologin())
+        {
+            $this->redirect($url);
+        }
+        
+        $this->view->assign('passwd_complexity_rule', $rule_info);
+        $this->view->assign('login_captcha', $login_captcha);
+        $this->view->assign('title', __('Login'));
+        $this->assignconfig('status',10);
+        Hook::listen("login_init", $this->request);
+        return $this->view->fetch();
+    }
+    
+    /**
+     * 注销登录
+     */
+    public function logout()
+    {
+        $this->auth->logout();
+        $this->redirect(url('index/login'));
+    }
     
     
 }
