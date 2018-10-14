79d4be9b7292aced6202e7e63e6a95c095880ce9
qinxuening <2423859713@qq.com>
2018-10-14 16:44:25 +0800

    推送列表编辑

---
+++ application/admin/controller/Push.php
@@ -0,0 +1,63 @@
+<?php
+/*
+*@author: qinxuening
+*@E-mail: 2423859713@qq.com
+*@date: 2018年10月14日 下午1:33:45
+*/
+namespace app\admin\controller;
+use app\common\controller\baseAdmin;
+use think\Db;
+
+class Push extends baseAdmin{
+
+    /**
+     * 推送列表
+     */
+    public function index($page = 0, $limit = 10) {
+        if ($this->request->isAjax())
+        {
+            $count = Db::name('push')->count('*');
+            $list = Db::name('push')->page($page,$limit)->select();
+            return json(['code' => 0, 'count' => $count, 'data' => $list,'msg' => '获取成功']);
+        }else{
+             return $this->view->fetch();
+        }
+    }
+    
+    /**
+     * 编辑
+     * @param string $ids
+     */
+    public function edit($ids = NULL) {
+        if ($this->request->isAjax()){
+            $id = input('post.id');
+            $data = input('');
+            $data['status'] = $data['status'] == 'on' ? '1' : 0;
+            $data['is_time_push'] = $data['is_time_push'] == 'on' ? '1' : 0;
+            if($id) {
+                $result = Db::name('push')->where(['id' => $id])->update($data);
+            } else {
+                unset($data['id']);
+                $result = Db::name('push')->where(['id' => $id])->insert($data);
+            }
+            
+            if(false !== $result){
+                return json(['code' => 1, 'status' => 'success', 'msg' => '操作成功']);
+            } else {
+                return json(['code' => -1, 'status' => 'error', 'msg' => '非法操作']);
+            }
+        } 
+        $list = Db::name('push')->where(['id' => $ids])->find();
+        $this->assign('list', $list);
+        return $this->view->fetch();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
