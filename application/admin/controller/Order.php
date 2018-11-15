4c7df9a56fd4d685b887974461479879d1bc8662
git <2423859713@qq.com>
2018-11-15 18:05:05 +0800

    上传测试

---
+++ application/admin/controller/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @author: qxn
+* @E-mial: 2423859713@qq.com
+* @date: 2018年11月15日 下午5:51:19
+*/
+namespace app\admin\controller;
+
+use app\common\controller\baseAdmin;
+
+class Order extends baseAdmin{
+    
+    public function index() {
+        return $this->view->fetch();
+    }
+}
