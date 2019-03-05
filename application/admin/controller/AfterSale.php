e6e9a144d1c11803f37cf75fe5af27077e4a5b5e
git <2423859713@qq.com>
2019-03-05 18:09:39 +0800

    11

---
+++ application/admin/controller/AfterSale.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author: qxn
+ * @E-mial: 2423859713@qq.com
+ * @date: 2018年12月1日 下午5:51:19
+ */
+namespace app\admin\controller;
+use app\common\controller\baseAdmin;
+use think\Queue;
+use think\Db;
+
+class AfterSale extends baseAdmin{
+    protected $table;
+    protected $excel_title;
+    protected $table_name;
+    protected $status;
+    
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->table = Db::name('jobs');
+        $this->table_name = 'jobs';
+        $this->excel_title = __('Job report');
+        $this->status = [];
+    }
+
+    public function med_diff_evalua(){
+        return $this->view->fetch();
+    }
+    
+    /**
+     * 入列测试，发布任务，生产者
+     */
+    public function emailJob () {
+        for ($i = 0; $i < 1000; $i++) {
+            $emailJobClassName  = 'app\job\EmailJob';
+            //如果一个任务类里有多个小任务的话，如上面的例子二，需要用@+方法名app\lib\job\Job2@task1、app\lib\job\Job2@task2
+            $jobQueueName = "emailJobQueue";
+            $jobData['config']=10;
+            $jobData['toemail']='242859713@qq.com';
+            $jobData['name']='qin';
+            $jobData['subject']='队列邮箱测试##################';
+            $isPushed =  Queue::push($emailJobClassName , $jobData , $jobQueueName);
+            //Queue::later($delay, $job, $data = '', $queue = null) //$delay秒后执行
+            if($isPushed) {
+                echo date('Y-m-d H:i:s') . " a Job is Pushed to the MQ"."<br>";
+            }
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
