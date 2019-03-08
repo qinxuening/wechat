59889f1f426d300a776379c57dacdf9165d419cf
qinxuening <2423859713@qq.com>
2019-03-09 00:35:49 +0800

    area单独模块

---
+++ public/static/modules/area.js
@@ -0,0 +1,52 @@
+layui.define(['jquery','form','we'], function(exports) {
+    var $ = layui.$
+        ,form = layui.form
+        , we = layui.we;
+    var area = {
+            /**
+         * 省份点击弹窗
+         */
+        init:function() {
+            $("body").on("click","#areaId",function (){
+                we.api.open_current_page_layer($(".layer-form"),'选择所在省份',{
+                },['80%' , '*']);
+                var areaId = [];
+                var areaIdCh = []
+                //选中省份
+                $("body").on("click",".submit",function (){
+                    $(".body-form input[type=checkbox]").each(function (index,value) {
+                        if($(this).is(':checked') && $(this).val() != 'on') {
+                            areaId.push($(this).val());
+                            areaIdCh.push($(this).attr('title'));
+                        }
+                    })
+                    $("#areaId").val(areaIdCh.join(','));
+                    $("#areaIdH").val(areaId.join(','));
+                    layer.closeAll();
+                });
+                /**
+                 * 重置
+                 * */
+                $("body").on("click",".reset",function (){
+                    layer.closeAll();
+                });
+            })
+
+
+            /**
+             *省份全选、反选
+             */
+            form.on('radio(check_all)', function(data){
+                var value = data.value;
+                if(value == 'all') {
+                    $(".location_address_layer").find("input[type=checkbox]").prop('checked', true);
+                    $(".location_address_layer").find(".layui-form-checkbox").addClass('layui-form-checked');
+                } else {
+                    $(".location_address_layer").find("input[type=checkbox]").prop('checked', false);
+                    $(".location_address_layer").find(".layui-form-checkbox").removeClass('layui-form-checked');
+                }
+            });
+        }
+    }
+    exports('area', area);
+});
